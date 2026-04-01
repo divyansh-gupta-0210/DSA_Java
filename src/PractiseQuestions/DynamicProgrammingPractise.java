e863ce2545ef94fa0c2be1336876a0907c0151c5
divyansh-gupta-0210 <divyanshgupta2108@gmail.com>
2026-04-02 02:51:18 +0530

    DP on grids

--- src/PractiseQuestions/DynamicProgrammingPractise.java
+++ src/PractiseQuestions/DynamicProgrammingPractise.java
@@ -34,18 +34,259 @@ public class DynamicProgrammingPractise {
 //             Arrays.fill(dp[i], -1);
 //        }
 //        print(uniquePaths(grid, dp, grid.length-1, grid[0].length - 1));
-        int[][] grid = {{0,0,0},{0,-1,0},{0,0,0}};
+//        int[][] grid = {{0,0,0},{0,-1,0},{0,0,0}};
+//        int[][] dp = new int[grid.length][grid[0].length];
+//        for(int i = 0; i < dp.length; i++){
+//            Arrays.fill(dp[i], -1);
+//        }
+//        print(gridMazeObstacles(grid, dp, grid.length, grid[0].length));
 
-        int[][] dp = new int[grid.length][grid[0].length];
-        for(int i = 0; i < dp.length; i++){
-            Arrays.fill(dp[i], -1);
+//        int[][] grid = {{10,8,2},{10,5,100},{1,1,2}};
+//
+//        int[][] dp = new int[grid.length][grid[0].length];
+//        for(int i = 0; i < dp.length; i++){
+//            Arrays.fill(dp[i], -1);
+//        }
+//        print(minimumPathSum(grid, dp, grid.length - 1, grid[0].length - 1));
+
+//        int[][] grid = {{1},{2,3},{3,6,7},{8,9,6,10}};
+//
+//        int[][] dp = new int[grid.length][];
+//        for (int i = 0; i < grid.length; i++) {
+//            dp[i] = new int[grid[i].length];
+//            Arrays.fill(dp[i], -1);
+//        }
+//        print(triangleFixedStartingAndVariableEndingPoint(grid, dp, 0, 0));
+
+//        int[][] grid = {
+//                {1,2,10,4},
+//                {100,3,2,1},
+//                {1,1,20,2},
+//                {1,2,2,1}};
+//        int[][] dp = new int[grid.length][];
+//        for (int i = 0; i < grid.length; i++) {
+//            dp[i] = new int[grid[i].length];
+//            Arrays.fill(dp[i], -1);
+//        }
+//        int max = Integer.MIN_VALUE;
+//        for (int j = 0; j < grid[0].length; j++) {
+//            max = Math.max(max, minimumMaximumFallingPathSum(grid, dp, grid.length - 1, j));
+//        }
+//        print(minimumMaximumFallingPathSum(grid, dp, grid.length - 1, grid[0].length - 1));
+//        System.out.println(max);
+
+        int[][] grid = {{2,3,1,2},{3,4,2,2},{5,6,3,5}};
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][][] dp = new int[n][m][m];
+        for (int i = 0; i < n; i++) {
+            for (int j1 = 0; j1 < m; j1++) {
+                Arrays.fill(dp[i][j1], -1);
+            }
         }
-        print(gridMazeObstacles(grid, dp, grid.length, grid[0].length));
+
+        System.out.println(cherryPickUp(grid, dp, 0, 0, m - 1));
+    }
+
+    public static int cherryPickUp(int[][] grid, int[][][] dp, int i, int j1, int j2){
+        int n = grid.length;
+        int m = grid[0].length;
+        if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) {
+            return Integer.MIN_VALUE;
+        }
+        if (i == n - 1) {
+            if (j1 == j2) return grid[i][j1];
+            else return grid[i][j1] + grid[i][j2];
+        }
+        if (dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxi = Integer.MIN_VALUE;
+        for (int dj1 = -1; dj1 <= 1; dj1++) {
+            for (int dj2 = -1; dj2 <= 1; dj2++) {
+
+                int value;
+                if (j1 == j2) value = grid[i][j1];
+                else value = grid[i][j1] + grid[i][j2];
+
+                value += cherryPickUp(grid, dp, i + 1, j1 + dj1, j2 + dj2);
+
+                maxi = Math.max(maxi, value);
+            }
+        }
+        return dp[i][j1][j2] = maxi;
+    }
+
+    public static int minimumMaximumFallingPathSum(int[][] grid, int[][] dp, int row, int col){
+
+        int[] prev = new int[grid.length];
+
+        for(int i = 0; i < grid.length; i++){
+            prev[i] = grid[0][i];
+        }
+
+        for(int i = 1; i < grid.length; i++){
+            int[] curr = new int[grid.length];
+            for(int j = 0; j < grid[0].length; j++){
+                int up = grid[i][j] + prev[j];
+                int ld = Integer.MIN_VALUE;
+                int rd = Integer.MIN_VALUE;
+                if(j - 1 >= 0){
+                    ld = grid[i][j] + prev[j - 1];
+                }
+                if(j + 1 < grid.length){
+                    rd = grid[i][j] + prev[j + 1];
+                }
+                curr[j] = Math.max(up, Math.max(ld, rd));
+            }
+
+            prev = curr;
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int j = 0; j < grid.length; j++) {
+            max = Math.max(max, prev[j]);
+        }
+        return max;
+
+//        Tabulation
+//        for(int j = 0; j < grid.length ; j++){
+//            dp[0][j] = grid[0][j];
+//        }
+//
+//        for(int i = 1; i < grid.length; i++){
+//            for(int j = 0; j < grid[i].length; j++){
+//                int ld = Integer.MIN_VALUE; int rd = Integer.MIN_VALUE;
+//                int up = grid[i][j] + dp[i-1][j];
+//                if(j - 1 >= 0) ld = grid[i][j] + dp[i-1][j-1];
+//                if(j + 1 < grid.length) rd = grid[i][j] + dp[i-1][j+1];
+//                dp[i][j] = Math.max(up, Math.max(ld, rd));
+//            }
+//        }
+//        int max = Integer.MIN_VALUE;
+//        for (int j = 0; j < grid[0].length; j++) {
+//            max = Math.max(max, dp[grid.length - 1][j]);
+//        }
+//        print(Arrays.toString(dp[dp.length-1]));
+//        return max;
+
+//        Brute and Better
+//        if(col < 0 || col >= grid[0].length){
+//            return Integer.MIN_VALUE;
+//        }
+//        if(row == 0){
+//            return grid[0][col];
+//        }
+//        if(dp[row][col] != -1) return dp[row][col];
+//        int up = grid[row][col] + minimumMaximumFallingPathSum(grid, dp, row - 1, col);
+//        int rightDi = grid[row][col] + minimumMaximumFallingPathSum(grid, dp, row - 1, col + 1);
+//        int leftDi = grid[row][col] + minimumMaximumFallingPathSum(grid, dp, row - 1, col - 1);
+//        return dp[row][col] = Math.max(up, Math.max(rightDi, leftDi));
+    }
+
+    public static int triangleFixedStartingAndVariableEndingPoint(int[][] grid, int[][] dp, int row, int col){
+
+//        Space Optimization
+        int[] prev = new int[grid[grid.length - 1].length];
+        for (int j = 0; j < grid[grid.length - 1].length; j++) {
+            prev[j] = grid[grid.length - 1][j];
+        }
+        for(int i = grid.length - 2; i >= 0; i--){
+            int[] curr = new int[i+1];
+            for(int j = 0; j <= i; j++){
+                int down = grid[i][j] + prev[j];
+                int dai = grid[i][j] + prev[j+1];
+                curr[j] = Math.min(dai, down);
+            }
+            prev = curr;
+        }
+        return prev[0];
+
+//        Tabulation
+//        for (int j = 0; j < grid[grid.length - 1].length; j++) {
+//            dp[grid.length - 1][j] = grid[grid.length - 1][j];
+//        }
+//
+//        for(int i = grid.length - 2; i >= 0; i--){
+//            for (int j = 0; j <= i; j++){
+//                int down = grid[i][j] + dp[i+1][j];
+//                int dia = grid[i][j] + dp[i+1][j+1];
+//                dp[i][j] = Math.min(down, dia);
+//            }
+//        }
+//        return dp[0][0];
+
+//        Brute + Better
+//        if(row == grid.length - 1){
+//            return grid[row][col];
+//        }
+//        if (dp[row][col] != -1) return dp[row][col];
+//        int down = grid[row][col] + triangleFixedStartingAndVariableEndingPoint(grid, dp, row + 1, col);
+//        int diagonal = grid[row][col] + triangleFixedStartingAndVariableEndingPoint(grid, dp, row + 1, col + 1);
+//        return dp[row][col] = Math.min(down, diagonal);
+    }
+
+    public static int minimumPathSum(int[][] grid, int[][] dp, int row, int col){
+
+        int[] prev = new int[col + 1];
+
+        for(int i = 0; i <= row; i++){
+            int[] curr = new int[col + 1];
+            for(int j = 0; j <= col; j++){
+                if(i == 0 && j == 0){
+                    curr[j] = grid[i][j];
+                }
+                else{
+                    int up = Integer.MAX_VALUE;
+                    int left = Integer.MAX_VALUE;
+
+                    if(i > 0) up = prev[j] + grid[i][j];
+                    if(j > 0) left = curr[j - 1] + grid[i][j];
+
+                    curr[j] = Math.min(up, left);
+                }
+            }
+            prev = curr;
+        }
+        return prev[col];
+        
+//        Tabulation
+//        for(int i = 0; i <= row; i++){
+//            for(int j = 0; j <= col; j++){
+//                if(i == 0 && j == 0){
+//                    dp[i][j] = grid[0][0];
+//                }
+//                else{
+//                    int up = Integer.MAX_VALUE;
+//                    int left = Integer.MAX_VALUE;
+//                    if(i > 0) up = grid[i][j] + dp[i-1][j];
+//                    if(j > 0) left = grid[i][j] + dp[i][j-1];
+//                    dp[i][j] = Math.min(up, left);
+//                }
+//            }
+//        }
+//        return dp[row][col];
+
+//        if(row == 0 && col == 0){
+//            return grid[0][0];
+//        }
+//        if(row < 0 || col < 0){
+//            return Integer.MAX_VALUE;
+//        }
+//        if(dp[row][col] != -1){
+//            return dp[row][col];
+//        }
+//        int up = minimumPathSum(grid, dp, row - 1, col);
+//        int left = minimumPathSum(grid, dp, row, col-1);
+//
+//        if(up != Integer.MAX_VALUE) up += grid[row][col];
+//        if(left != Integer.MAX_VALUE) left += grid[row][col];
+//
+//        return dp[row][col] = Math.min(up, left);
     }
 
     public static int gridMazeObstacles(int[][] grid, int[][] dp, int row, int col){
 
-//        Optimal - Space Optimization
+//      Optimal - Space Optimization
         int[] prev = new int[row];
         for(int i = 0; i < row; i++){
             int[] curr = new int[row];
@@ -70,6 +311,7 @@ public class DynamicProgrammingPractise {
             prev = curr;
         }
         return prev[grid.length-1];
+
 //        Optimal - Tabulation
 //        for(int i = 0; i < row; i++){
 //            for(int j = 0; j < col; j++){
@@ -105,8 +347,8 @@ public class DynamicProgrammingPractise {
 //            return dp[row][col];
 //        }
 //        int up = gridMazeObstacles(grid, dp, row -1, col);
-//        int down = gridMazeObstacles(grid, dp, row, col - 1);
-//        return dp[row][col] = up + down;
+//        int left = gridMazeObstacles(grid, dp, row, col - 1);
+//        return dp[row][col] = up + left;
     }
 
     public static int uniquePaths(int[][] grid, int[][] dp, int row, int col){
